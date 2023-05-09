cddb8f2c648425a347ceb1e90d023112f1fae890
Yur3355 <114501342+Yur3355@users.noreply.github.com>
2023-05-10 02:44:37 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,23 +2,6 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
-TEST(task1, test1) {
-    Automata a = Automata();
-    a.on();
-    a.coin(100);
-    int result = a.getState();
-    EXPECT_EQ(2, result);
-}
-
-TEST(task1, test2) {
-    Automata a = Automata();
-    a.on();
-    a.coin(100);
-    a.choice(5);
-    int result = a.getState();
-    EXPECT_EQ(2, result);
-}
-
 TEST(task2, test3) {
     Automata a = Automata();
     a.on();
